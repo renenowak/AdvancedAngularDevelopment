eeaa448cde8ea9bf9597a0ff2f8b2fac57a6070b
Rene Nowak <nowak.rene@gmail.com>
2020-08-10 13:36:42 +0200

    add primary class

--- 01-Theming/ngThemeApp/src/app/demos/demos.module.ts
+++ 01-Theming/ngThemeApp/src/app/demos/demos.module.ts
@@ -1,34 +1,34 @@
-import { CommonModule } from "@angular/common";
-import { HttpClient, HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { RouterModule, Routes } from "@angular/router";
-import { MarkdownModule } from "ngx-markdown";
-import { MaterialModule } from "../material.module";
-import { DemoContainerComponent } from "./demo-container/demo-container.component";
-import { DemoService } from "./demo.service";
-import { StatefulComponent } from "./samples/stateful/stateful.component";
-import { SkillsService } from "../model/skills/skills.service";
-import { MarkdownEditorComponent } from "./markdown-editor/markdown-editor.component";
-import { CardComponent } from "./samples/card/card.component";
-import { ContentProjectionComponent } from "./samples/content-projection/content-projection.component";
-import { PopupContainerComponent } from "./samples/popup-container/popup-container.component";
-import { PopupComponent } from "./samples/popup-container/popup/popup.component";
-import { SplitSampleComponent } from "./samples/content-projection/split-sample/split-sample.component";
-import { UxModule } from "../ux/ux.module";
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
+import { MaterialModule } from '../material.module';
+import { DemoContainerComponent } from './demo-container/demo-container.component';
+import { DemoService } from './demo.service';
+import { StatefulComponent } from './samples/stateful/stateful.component';
+import { SkillsService } from '../model/skills/skills.service';
+import { MarkdownEditorComponent } from './markdown-editor/markdown-editor.component';
+import { CardComponent } from './samples/card/card.component';
+import { ContentProjectionComponent } from './samples/content-projection/content-projection.component';
+import { PopupContainerComponent } from './samples/popup-container/popup-container.component';
+import { PopupComponent } from './samples/popup-container/popup/popup.component';
+import { SplitSampleComponent } from './samples/content-projection/split-sample/split-sample.component';
+import { UxModule } from '../ux/ux.module';
 
 const demoRoutes: Routes = [
   {
-    path: "",
+    path: '',
     component: DemoContainerComponent,
 
     children: [
-      { path: "stateful", component: StatefulComponent },
-      { path: "card", component: CardComponent },
-      { path: "projection", component: ContentProjectionComponent },
-      { path: "popup", component: PopupContainerComponent }
-    ]
-  }
+      { path: 'stateful', component: StatefulComponent },
+      { path: 'card', component: CardComponent },
+      { path: 'projection', component: ContentProjectionComponent },
+      { path: 'popup', component: PopupContainerComponent },
+    ],
+  },
 ];
 
 @NgModule({
@@ -40,7 +40,7 @@ const demoRoutes: Routes = [
     PopupContainerComponent,
     PopupComponent,
     MarkdownEditorComponent,
-    SplitSampleComponent
+    SplitSampleComponent,
   ],
   imports: [
     CommonModule,
@@ -51,10 +51,10 @@ const demoRoutes: Routes = [
     MaterialModule,
     HttpClientModule,
     MarkdownModule.forRoot({
-      loader: HttpClient
-    })
+      loader: HttpClient,
+    }),
   ],
   providers: [DemoService, SkillsService],
-  entryComponents: [SplitSampleComponent, PopupComponent]
+  entryComponents: [SplitSampleComponent, PopupComponent],
 })
 export class DemosModule {}
